f8ec3b989b48a4555e08d349aeec4347ba280154
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-10-26 21:28:02 +0100

    Convert to coordinates

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,15 +1,25 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
+#include <TinyGPS++.h>
 
 HardwareSerial GPS_Serial(2);
+TinyGPSPlus gps;
 
 void setup() {
-  Serial.begin(115200);
-  GPS_Serial.begin(9600, SERIAL_8N1, 2, 15);
+    Serial.begin(115200);
+    GPS_Serial.begin(9600, SERIAL_8N1, 2, 15);
+    Serial.println(F("GPS Ready"));
 }
 
 void loop() {
-  while (GPS_Serial.available()) {
-    Serial.write(GPS_Serial.read());
-  }
+    while (GPS_Serial.available() > 0) {
+        if (gps.encode(GPS_Serial.read())) {
+            if (gps.location.isValid()) {
+                Serial.print(F("Lat: "));
+                Serial.println(gps.location.lat(), 6);
+                Serial.print(F("Lng: "));
+                Serial.println(gps.location.lng(), 6);
+            }
+        }
+    }
 }
