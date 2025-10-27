07b49dbb18a4783deef9d89ba34ac87aa22704be
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-10-27 10:44:10 +0100

    Slower printing

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -2,70 +2,86 @@
 #include <HardwareSerial.h>
 #include <TinyGPS++.h>
 
+const int GPS_RX_PIN = 4;
+const int GPS_TX_PIN = 15;
+const unsigned long PRINT_INTERVAL = 3000UL;
+const unsigned long SERIAL_BAUD = 115200;
+const unsigned long GPS_BAUD = 9600;
 HardwareSerial GPS_Serial(2);
 TinyGPSPlus gps;
+unsigned long lastPrint = 0;
+
+void printPadded(int value) {
+    if (value < 10) Serial.print(F("0"));
+    Serial.print(value);
+}
+
+void printGPSData() {
+    if (gps.location.isValid()) {
+        Serial.print(F("Lat: "));
+        Serial.println(gps.location.lat(), 6);
+        Serial.print(F("Lon: "));
+        Serial.println(gps.location.lng(), 6);
+        Serial.print(F("Last fix age: "));
+        Serial.print(gps.location.age());
+        Serial.println(F("ms"));
+    }
+    if (gps.altitude.isValid()) {
+        Serial.print(F("Alt: "));
+        Serial.print(gps.altitude.meters());
+        Serial.println(F("m"));
+    }
+    if (gps.speed.isValid()) {
+        Serial.print(F("Speed: "));
+        Serial.print(gps.speed.kmph());
+        Serial.println(F(" km/h"));
+    }
+    if (gps.course.isValid()) {
+        Serial.print(F("Course: "));
+        Serial.print(gps.course.deg());
+        Serial.println(F(" deg"));
+    }
+    if (gps.time.isValid()) {
+        printPadded(gps.time.hour());
+        Serial.print(F(":"));
+        printPadded(gps.time.minute());
+        Serial.print(F(":"));
+        printPadded(gps.time.second());
+        Serial.println();
+    }
+    if (gps.date.isValid()) {
+        Serial.print(F("Date: "));
+        Serial.print(gps.date.year());
+        Serial.print(F("/"));
+        printPadded(gps.date.month());
+        Serial.print(F("/"));
+        printPadded(gps.date.day());
+        Serial.println();
+    }
+    if (gps.satellites.isValid()) {
+        Serial.print(F("Sats: "));
+        Serial.println(gps.satellites.value());
+    }
+    if (gps.hdop.isValid()) {
+        Serial.print(F("HDOP: "));
+        Serial.println(gps.hdop.hdop(), 2);
+    }
+}
 
 void setup() {
-    Serial.begin(115200);
-    GPS_Serial.begin(9600, SERIAL_8N1, 4, 15);
+    Serial.begin(SERIAL_BAUD);
+    GPS_Serial.begin(GPS_BAUD, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
     Serial.println(F("GPS Ready"));
 }
 
 void loop() {
     while (GPS_Serial.available() > 0) {
         if (gps.encode(GPS_Serial.read())) {
-            if (gps.location.isValid()) {
-                Serial.print(F("Lat: "));
-                Serial.println(gps.location.lat(), 6);
-                Serial.print(F("Lon: "));
-                Serial.println(gps.location.lng(), 6);
+            if (gps.location.isValid() && (lastPrint == 0 || millis() - lastPrint >= PRINT_INTERVAL)) {
+                printGPSData();
+                Serial.println(F("-"));
+                lastPrint = millis();
             }
-            if (gps.altitude.isValid()) {
-                Serial.print(F("Alt: "));
-                Serial.print(gps.altitude.meters());
-                Serial.println(F("m"));
-            }
-            if (gps.speed.isValid()) {
-                Serial.print(F("Speed: "));
-                Serial.print(gps.speed.kmph());
-                Serial.println(F(" km/h"));
-            }
-            if (gps.course.isValid()) {
-                Serial.print(F("Course: "));
-                Serial.print(gps.course.deg());
-                Serial.println(F(" deg"));
-            }
-            if (gps.time.isValid()) {
-                if (gps.time.hour() < 10) Serial.print(F("0"));
-                Serial.print(gps.time.hour());
-                Serial.print(F(":"));
-                if (gps.time.minute() < 10) Serial.print(F("0"));
-                Serial.print(gps.time.minute());
-                Serial.print(F(":"));
-                if (gps.time.second() < 10) Serial.print(F("0"));
-                Serial.println(gps.time.second());
-            }
-            if (gps.date.isValid()) {
-                Serial.print(F("Date: "));
-                Serial.print(gps.date.year());
-                Serial.print(F("/"));
-                if (gps.date.month() < 10) Serial.print(F("0"));
-                Serial.print(gps.date.month());
-                Serial.print(F("/"));
-                if (gps.date.day() < 10) Serial.print(F("0"));
-                Serial.println(gps.date.day());
-            }
-            if (gps.satellites.isValid()) {
-                Serial.print(F("Sats: "));
-                Serial.println(gps.satellites.value());
-            }
-            if (gps.hdop.isValid()) {
-                Serial.print(F("HDOP: "));
-                Serial.println(gps.hdop.hdop());
-            }
-            Serial.print(F("Last fix age: "));
-            Serial.print(gps.location.age());
-            Serial.println(F("ms"));
         }
     }
 }
